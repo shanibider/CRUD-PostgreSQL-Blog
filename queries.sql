9d30ce2eeb65992ea11fd2439076f476ff25354f
Shani Bider <shanibider@gmail.com>
2024-03-07 17:25:36 +0200

    modify POST and GET requests to connect to my database

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@ CREATE TABLE posts (
   id SERIAL PRIMARY KEY,
   subject VARCHAR(100) NOT NULL,
   title VARCHAR(100) NOT NULL,
-  content VARCHAR(100) NOT NULL
+  content VARCHAR(1000) NOT NULL
 );
 
 INSERT INTO posts (subject, title, content) VALUES ('Fitness', 'How to get fit', 'Eat healthy and exercise');
